c6c697eac2cb8a2f64db8f7efb02cf93e752d732
CloudsWeight <CloudsWeight@users.noreply.github.com>
2021-11-17 13:15:18 -0800

    Update casino.py

--- casino.py
+++ casino.py
@@ -27,41 +27,59 @@ def main():
     dealer = Dealer()
     dealer.name = 'Bob'
     print(f"{dealer.name} will be working at {casino.name} tonight")
-
     # create the players at the table
     craps = Craps(dealer)
-
     #craps.player_prompt()
     players = [Player(50,'Larry')]
     rolls = []
     db = {}
+    Larry = players[0] # easier to remember for test
 
-    print(f"{players[0].name} gets the dice first")
-
-    number = int(input("What's the number to bet on?: "))
-
-    bet = int(input("How much are you betting?: "))
-    come_bet = int(input("How much on the pass line?"))
-    come_bet *2
-    craps.roll()
-    point = craps.roll
-    print(craps.roll)
-    hard = craps.hard
-    dealer.collect_bet(players[0].bet(come_bet,2), craps.roll)
-    print("Come out roll commence!")
-    i = 0
-    if craps.roll == 7:
-        i = 0
-        dealer.pay(players[0],come_bet)
-        print("7, winner!")
-    else:
+    ans = input("Do you want to play? [Y/N]: ")
+    ans = ans.upper()
+    print(ans)
+    while ans == 'Y':
+        # Loop game
+        # number = int(input("What's the number to bet on?: "))
+        # bet = int(input("How much are you betting?: "))
+        #come_bet = int(input("How much on the pass line?"))
+        # create craps point
         craps.roll()
-        print(craps.roll)
-    bet_win = craps.hard_way(bet)
-    bet = players[0].bet(bet, number)
-    if players[0].number == craps.roll and craps.hard == True:
-        Dealer.pay(players[0], bet_win)
-    print(players[0].money)
+
+        print(craps.roll, craps.d1, craps.d2, craps.hard)
+
+        # craps.hard is boolean
+        # True = hard ways / False = not hard ways
+        bet = int(input("How much to bet on the pass line?: "))
+        # dealer.collect_bet() method
+        # collect_bet takes the number to bet on, and the amount to bet
+        # number is the 'point'
+        # amount is user input
+        # point and craps have not happened yet
+        point = False
+        crappy_roll = False
+        b = None
+        while b == None:
+            print("Select another bet below: ")
+            print("[1] Hard Ways bet")
+            b = 1
+        if b == 1:
+            print("Hard ways bet")
+            number = int(input("What's the number to bet on?: "))
+            hw_amount = int(input("How much are you betting?: "))
+            hard_way_bet = dealer.collect_bet(number, hw_amount)
+        print("Come out roll commence!")
+        i = 0
+        while point == False or crappy_roll == False:
+            if craps.roll == 7 and i == 0:
+                dealer.pay(Larry, dealer.passline_win(bet))
+                print("7, winner!")
+            else:
+                pass
+        print(type(bet_win), bet_win)
+        print(players[0].money)
+
+
 
 
 if __name__ == '__main__':
