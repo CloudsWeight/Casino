52f75908f848630d388e8e1883bfaacd49f8a25d
CloudsWeight <CloudsWeight@users.noreply.github.com>
2021-11-25 12:49:54 -0800

    Update Craps.py

--- Craps.py
+++ Craps.py
@@ -2,13 +2,14 @@ from Dice import Dice
 from Player import Player
 from Dealer import Dealer
 from Bets import Bets
-from randos import create_rando_players, randodict, randomoney
+import randos
 
 class Craps:
     ''' '''
     def __init__(self):
         self.roll = {}
-        self.players = create_rando_players()
+        self.players = randos.make_ten_players()
+        self.point = False
 
     def list_players(self):
         players = []
@@ -16,15 +17,25 @@ class Craps:
             players.append({"name":i.name, "money":i.money})
         return players
 
-    def select_random_players(self):
-        print(len(self.players))
+    def automate_players(self):
+
 
     def throw_dice(self):
         self.roll = Dice().roll()
         self.d1 = self.roll["d1"]
         self.d2 = self.roll["d2"]
         self.total = self.d1 + self.d2
+        if self.d1 == self.d2:
+            self.hard == True
+        else:
+            self.hard = False
         return self.total
+
+    def roll_for_point(self):
+        if self.point == False:
+            self.throw_dice()
+            return (f"{self.total, self.d1 self.d2} Hard ways is (self.hard}"")
+
 
     def ask_to_play(self):
         ans = input("Play craps? [Y]es or [N]o: ").upper()
@@ -38,7 +49,9 @@ class Craps:
 
 def main():
     craps = Craps()
-    print(craps.list_players())
+    print("Here are the current players at the table")
+    for i in craps.players:
+        print(f"{i.name} has ${i.money}.00")
 
 if __name__ == "__main__":
     main()
