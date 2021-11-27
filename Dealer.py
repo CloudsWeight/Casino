53fc6945c6082d62431cd0eff8176e118ef55206
CloudsWeight <CloudsWeight@users.noreply.github.com>
2021-11-27 04:24:17 -0800

    Update Dealer.py

--- Dealer.py
+++ Dealer.py
@@ -6,23 +6,28 @@ from randos import names
 from random import randint
 
 names = names
+
 def random_dealer_name():
     n = randint(0, (len(names)-1))
     return names[n]
+
+def make_random_dealer():
+    dealer = Dealer()
+    dealer.name = random_dealer_name()
+    return dealer
 
 class Dealer:
 
     def __init__(self):
         self.money = 1000000
-        self.name = random_dealer_name()
+        self.name = None
 
     def set_token(self, token):
         return token
 
-    def collect_bet(self, number, amount):
+    def collect_bet(self, amount=0):
         self.money = self.money + amount
-        self.number = number
-        return amount, number
+        return amount
 
     #def ask_bet(self):
 
@@ -34,10 +39,8 @@ class Dealer:
         bet = int(bet)
         return 30 * bet
 
-    def pay(self, player, win):
-        win = win
+    def pay(self, win):
         self.money - win
-        player.money
         return win
 
 def main():
